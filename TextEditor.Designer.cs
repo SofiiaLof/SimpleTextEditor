c799f7f532d7e931ba93cd1cfea9afe7a34131e1
Sofiia Löf <sofiia.lof@utb.ecutbildning.se>
2022-04-14 16:14:08 +0200

    Add project files.

---
+++ TextEditor.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace SimpleTextEditor
+{
+    partial class TextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveAsFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.labelTecken = new System.Windows.Forms.Label();
+            this.labelTeckenUtanM = new System.Windows.Forms.Label();
+            this.ordAntalCount = new System.Windows.Forms.Label();
+            this.labelLinesCount = new System.Windows.Forms.Label();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(555, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newMenuItem,
+            this.openFileMenuItem,
+            this.toolStripSeparator1,
+            this.saveFileMenuItem,
+            this.toolStripSeparator3,
+            this.saveAsFileMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // newMenuItem
+            // 
+            this.newMenuItem.Name = "newMenuItem";
+            this.newMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.newMenuItem.Text = "New";
+            this.newMenuItem.Click += new System.EventHandler(this.newMenuItem_Click);
+            // 
+            // openFileMenuItem
+            // 
+            this.openFileMenuItem.Name = "openFileMenuItem";
+            this.openFileMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openFileMenuItem.Text = "Open";
+            this.openFileMenuItem.Click += new System.EventHandler(this.openFileMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(109, 6);
+            // 
+            // saveFileMenuItem
+            // 
+            this.saveFileMenuItem.Name = "saveFileMenuItem";
+            this.saveFileMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveFileMenuItem.Text = "Save";
+            this.saveFileMenuItem.Click += new System.EventHandler(this.saveFileMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(109, 6);
+            // 
+            // saveAsFileMenuItem
+            // 
+            this.saveAsFileMenuItem.Name = "saveAsFileMenuItem";
+            this.saveAsFileMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveAsFileMenuItem.Text = "Save as";
+            this.saveAsFileMenuItem.Click += new System.EventHandler(this.saveAsFileMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(12, 27);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox.Size = new System.Drawing.Size(507, 348);
+            this.textBox.TabIndex = 2;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // labelTecken
+            // 
+            this.labelTecken.AutoSize = true;
+            this.labelTecken.Location = new System.Drawing.Point(13, 380);
+            this.labelTecken.Name = "labelTecken";
+            this.labelTecken.Size = new System.Drawing.Size(162, 13);
+            this.labelTecken.TabIndex = 3;
+            this.labelTecken.Text = "Antal bokstäver med mellanslag: ";
+            // 
+            // labelTeckenUtanM
+            // 
+            this.labelTeckenUtanM.AutoSize = true;
+            this.labelTeckenUtanM.Location = new System.Drawing.Point(13, 402);
+            this.labelTeckenUtanM.Name = "labelTeckenUtanM";
+            this.labelTeckenUtanM.Size = new System.Drawing.Size(163, 13);
+            this.labelTeckenUtanM.TabIndex = 4;
+            this.labelTeckenUtanM.Text = "Antal bokstäver  utan mellanslag:";
+            // 
+            // ordAntalCount
+            // 
+            this.ordAntalCount.AutoSize = true;
+            this.ordAntalCount.Location = new System.Drawing.Point(332, 380);
+            this.ordAntalCount.Name = "ordAntalCount";
+            this.ordAntalCount.Size = new System.Drawing.Size(52, 13);
+            this.ordAntalCount.TabIndex = 5;
+            this.ordAntalCount.Text = "Antal ord:";
+            // 
+            // labelLinesCount
+            // 
+            this.labelLinesCount.AutoSize = true;
+            this.labelLinesCount.Location = new System.Drawing.Point(335, 410);
+            this.labelLinesCount.Name = "labelLinesCount";
+            this.labelLinesCount.Size = new System.Drawing.Size(61, 13);
+            this.labelLinesCount.TabIndex = 6;
+            this.labelLinesCount.Text = "Antal rader:";
+            // 
+            // TextEditor
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 447);
+            this.Controls.Add(this.labelLinesCount);
+            this.Controls.Add(this.ordAntalCount);
+            this.Controls.Add(this.labelTeckenUtanM);
+            this.Controls.Add(this.labelTecken);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "TextEditor";
+            this.Text = "TextEditor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TextEditor_FormClosing);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem openFileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsFileMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label labelTecken;
+        private System.Windows.Forms.Label labelTeckenUtanM;
+        private System.Windows.Forms.Label ordAntalCount;
+        private System.Windows.Forms.Label labelLinesCount;
+    }
+}
+
---
+++ TextEditor.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+namespace SimpleTextEditor
+{
+
+    public partial class TextEditor : Form
+    {
+        public TextEditor()
+        {
+            InitializeComponent();
+            this.FormClosing += TextEditor_FormClosing;
+
+        }
+
+        bool textChanged = false;
+        string currentFile = "";
+
+
+
+        //function HandleChanges() handle changes int text
+
+        private void HandleChanges()
+        {
+            string sokvag = this.Text = Path.GetFileName(currentFile);
+            if (sokvag.Length > 0)
+            {
+                if (textChanged == true)
+                {
+                    this.Text = "*" + sokvag + "-TextEditor";
+                }
+                else
+                {
+                    this.Text = sokvag + "-TextEditor";
+                }
+            }
+            else
+            {
+
+                if (textChanged == true)
+                {
+                    this.Text = "* dok1.txt";
+                }
+                else
+                {
+                    this.Text =  "TextEditor";
+                }
+            }
+
+
+            string userInput = textBox.Text;
+
+            //Count letters (space included)
+
+            int count = 0;
+            foreach (char letter in userInput)
+            {
+                if (char.IsLetter(letter))
+                {
+                    if (!char.IsWhiteSpace(letter))
+                    {
+                        count++;
+                    }
+                }
+            }
+            labelTecken.Text = "Antal bokstäver med mellanslag: " + count.ToString();
+
+
+            //Count letters (space excluded)
+            int countchar = 0;
+            foreach (char tecken in userInput)
+            {
+                if (char.IsLetter(tecken))
+                { countchar++; }
+
+
+                if (char.IsWhiteSpace(tecken))
+                { countchar++; }
+
+
+            }
+            labelTeckenUtanM.Text = "Antal bokstäver utan mellanslag: " + countchar.ToString();
+
+            //Count words
+
+            char[] mellanslag = { ' ' };
+            int wordsCount = userInput.Split(mellanslag, StringSplitOptions.RemoveEmptyEntries).Length;
+            ordAntalCount.Text = "Antal ord: " + wordsCount.ToString();
+
+            //Count lines
+            int linescount = textBox.Lines.Length;
+            labelLinesCount.Text = "Antal rader: " + linescount.ToString();
+
+        }
+
+
+        private void newMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (textChanged == true)
+            {
+                switch (MessageBox.Show(
+                  "Vill du spara ändringar?",
+                  "Spara ändringar",
+                  MessageBoxButtons.YesNoCancel))
+                {
+                    case DialogResult.Yes:
+                        if (currentFile.Length > 0)
+                        {
+                            File.WriteAllText(currentFile, textBox.Text);
+                            textChanged = false;
+
+                        }
+
+                        break;
+                    case DialogResult.No:
+                        textBox.Text = "";
+                        break;
+                    case DialogResult.Cancel:
+
+                        break;
+                }
+            }
+
+
+            currentFile = "";
+            textChanged = false;
+            HandleChanges();
+
+        }
+
+
+        //Open file dialog (Open-file item)
+
+        private void openFileMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (textChanged == true)
+            {
+                switch (MessageBox.Show(
+                  "Vill du spara ändringar?",
+                  "Spara ändringar",
+                  MessageBoxButtons.YesNoCancel))
+                {
+                    case DialogResult.Yes:
+                        if (currentFile.Length > 0)
+                        {
+                            File.WriteAllText(currentFile, textBox.Text);
+                            textChanged = false;
+
+                        }
+
+                        break;
+                    case DialogResult.No:
+                        textBox.Text = "";
+                        break;
+                    case DialogResult.Cancel:
+
+                        break;
+                }
+            }
+
+
+            OpenFileDialog dialogOpen = new OpenFileDialog();
+
+            dialogOpen.Filter = "Text files|*.txt";
+            dialogOpen.Title = "Öppna fil";
+
+            if (dialogOpen.ShowDialog(this) == DialogResult.OK)
+            {
+                string filInnehall = File.ReadAllText(dialogOpen.FileName);
+
+                currentFile = dialogOpen.FileName;
+                if (filInnehall.Length > 0)
+                {
+                    textBox.Text = filInnehall;
+                    textChanged = false;
+                }
+            }
+            dialogOpen.Dispose();
+            HandleChanges();
+
+
+        }
+
+
+        //Function saves text if not saved earlier (Save as- menu item)
+        
+        private void saveAsFileMenuItem_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog dialogSave = new SaveFileDialog();
+            dialogSave.Filter = "Text files|*.txt";
+            dialogSave.Title = "Save as";
+
+
+            if (dialogSave.ShowDialog(this) == DialogResult.OK)
+            {
+                currentFile = dialogSave.FileName;
+
+                File.WriteAllText(currentFile, textBox.Text);
+                textChanged = false;
+
+            }
+
+            HandleChanges();
+
+        }
+
+
+
+       //Saves file that has been saved earlier (Save-menu item)
+
+        private void saveFileMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textChanged==true)
+            {
+                if (currentFile.Length > 0)
+                {
+                    File.WriteAllText(currentFile, textBox.Text);
+                    textChanged = false;
+
+                }
+                else
+                {
+
+                    SaveFileDialog dialogSave = new SaveFileDialog();
+                    dialogSave.Filter = "Text files|*.txt";
+                    dialogSave.Title = "Save as";
+
+
+                    if (dialogSave.ShowDialog(this) == DialogResult.OK)
+                    {
+                        currentFile = dialogSave.FileName;
+
+                        File.WriteAllText(currentFile, textBox.Text);
+                        textChanged = false;
+
+                    }
+
+                    HandleChanges();
+
+                }
+            }
+
+            HandleChanges();
+        }
+
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+            textChanged = true;
+            HandleChanges();
+
+
+        }
+
+
+        //Close form, user will be asked to save the file if not saved and changes have been made
+        
+        private void TextEditor_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (textChanged == true)
+            {
+
+                if (textChanged == true)
+                {
+                    switch (MessageBox.Show(
+                      "Vill du spara ändringar?",
+                      "Spara ändringar",
+                      MessageBoxButtons.YesNoCancel))
+                    {
+                        case DialogResult.Yes:
+                            if (currentFile.Length > 0)
+                            {
+                                File.WriteAllText(currentFile, textBox.Text);
+                                textChanged = false;
+
+                            }
+
+                            break;
+                        case DialogResult.No:
+                            textBox.Text = "";
+                            break;
+                        case DialogResult.Cancel:
+
+                            break;
+                    }
+                }
+
+            }
+
+
+        }
+
+        //Close- menu item, works same as above
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textChanged == true)
+            {
+
+                if (textChanged == true)
+                {
+                    switch (MessageBox.Show(
+                      "Vill du spara ändringar?",
+                      "Spara ändringar",
+                      MessageBoxButtons.YesNoCancel))
+                    {
+                        case DialogResult.Yes:
+                            if (currentFile.Length > 0)
+                            {
+                                File.WriteAllText(currentFile, textBox.Text);
+                                textChanged = false;
+                            }
+
+                            break;
+                        case DialogResult.No:
+                            this.Close();
+                            break;
+                        case DialogResult.Cancel:
+
+                            break;
+                    }
+                }
+
+            }
+
+            this.Close();
+
+        }
+
+
+    }
+}
+
